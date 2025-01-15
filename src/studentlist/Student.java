b2fed494c10fff766bb58ee6186125af3252d42c
AMAZEN AMAZEN <amazen@sheridancollege.ca>
2025-01-15 14:11:12 -0500

    this is my initial code

---
+++ src/studentlist/Student.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author fagun at 12/05/2024
+ */
+public class Student {
+    private String name;
+    public int studentID;
+    private String address;
+
+    public String getName() {
+        return name;
+    }//geter method to get the data
+
+    public void setName(String name) {
+        this.name = name;
+    }
+   //setter method to set the value of method.
+}
